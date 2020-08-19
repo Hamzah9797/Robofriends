ba5b5153b9f3e854dad3cf2d212878eb151e078f
Hamzah Hameed <hamzahthegreat@yahoo.com>
2020-08-19 13:55:15 +0300

    Updates

---
+++ precache-manifest.9e9f106be1d312579c78dfca99855d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618fa58c57db0aa12b46972c476a0237",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "776627562b3de6190524",
+    "url": "/Robofriends/static/css/main.c6ffe615.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776627562b3de6190524",
+    "url": "/Robofriends/static/js/main.5fce5e4c.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
